6571a91bc31ab9cf04c4fa720f5b467b2cbd6dfb
ilyaskoccc <ilyas_1997koc@hotmail.com>
2024-10-25 23:03:41 +0300

    ilk kurulum dosyaları oluşturuldu. userId düzenlendi.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,22 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        System.out.println("Hello world!");
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        System.out.println("Hello world!");
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        System.out.println("Hello world!");
     }
 
     public static double area(double width, double height) {
-       
+        System.out.println("Hello world!");
     }
 
     public static double area(double radius) {
-       
+        System.out.println("Hello world!");
     }
 }
