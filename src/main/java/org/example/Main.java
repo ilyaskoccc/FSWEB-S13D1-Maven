cf1acfbf7d3e05988e533c1ed25655aa4db5cbdd
ilyaskoccc <ilyas_1997koc@hotmail.com>
2024-10-25 23:51:33 +0300

    1. Soru Tamamlandı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,26 +2,44 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        /* 1. Soru Bölümü */
+    System.out.println(shouldWakeUp (true, 1));
+    System.out.println(shouldWakeUp (false, 2)); 
+    System.out.println(shouldWakeUp (true, 8)); 
+    System.out.println(shouldWakeUp (true, -1));
+
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        System.out.println("Hello world!");
+        if (isBarking) { 
+            if (clock >= 0 && clock <= 23) { 
+                if (clock >= 8 && clock <= 20) { 
+                    return false;
+                } else{
+                    return true;
+                }
+            } else { 
+                return false;
+            }
+        } else {
+            return false; 
+        }
     }
 
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-        System.out.println("Hello world!");
-    }
+    // public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
+    //     System.out.println("Hello world!");
+    // }
 
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-        System.out.println("Hello world!");
-    }
+    // public static boolean isCatPlaying(boolean isSummer, int temp) {
+    //     System.out.println("Hello world!");
+    // }
 
-    public static double area(double width, double height) {
-        System.out.println("Hello world!");
-    }
+    // public static double area(double width, double height) {
+    //     System.out.println("Hello world!");
+    // }
 
-    public static double area(double radius) {
-        System.out.println("Hello world!");
-    }
+    // public static double area(double radius) {
+    //     System.out.println("Hello world!");
+    // }
 }
